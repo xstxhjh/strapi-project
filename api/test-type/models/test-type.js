d8f3557f186bc200cd19639392bff61c670b66fa
xstxhjh <573363596@qq.com>
2021-06-04 15:40:40 +0800

    customize this model

--- api/test-type/models/test-type.js
+++ api/test-type/models/test-type.js
@@ -5,4 +5,11 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  // Before saving a value.
+  // Fired before an `insert` or `update` query.
+  beforeSave: (model, attrs, options) => {},
+  // After saving a value.
+  // Fired after an `insert` or `update` query.
+  afterSave: (model, attrs, options) => {},
+};
--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,13 @@
 module.exports = {
   settings: {
-    // ...
+    timeout: 1000,
+    load: {
+      before: ["timer", "responseTime", "logger", "cors", "responses", "gzip"],
+      order: [],
+      after: ["parser", "router"],
+    },
     hjh: {
       enabled: true,
     },
-  }
+  },
 };
